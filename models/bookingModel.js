a4a7886f7e91ed091d7385e8feee78726325527e
Khaledsamy31 <ksamy3166@gmail.com>
2024-11-15 13:23:37 +0000

    little changes

--- models/bookingModel.js
+++ models/bookingModel.js
@@ -4,7 +4,7 @@
     visitorId: {
       type: String,
       minlength: [5, "Visitor ID must be at least 5 characters"], // الحد الأدنى
-      maxlength: [120, "Visitor ID must not exceed 120 characters"], // الحد الأقصى
+      maxlength: [200, "Visitor ID must not exceed 200 characters"], // الحد الأقصى
       index: true,
       },
     userId: {
@@ -15,14 +15,15 @@
       type: String,
       required: [true, "Name is required"],
       minlength: [2, "Name must be at least 2 characters"], // الحد الأدنى
-      maxlength: [30, "Name must not exceed 30 characters"], // الحد الأقصى
+      maxlength: [40, "Name must not exceed 40 characters"], // الحد الأقصى
       },
     phoneNumber: {
       type: String,
         required: [true, "Phone number is required"],
         minlength: [11, "Phone number must be exactly 11 digits"], // الحد الأدنى
         maxlength: [11, "Phone number must be exactly 11 digits"], // الحد الأقصى
-        match: [/^\d+$/, "Phone number must contain only digits"],
+        match: [/^[\d\u0660-\u0669]+$/, "Phone number must contain only digits"],
+
       },
     date: { 
       type: Date, 
